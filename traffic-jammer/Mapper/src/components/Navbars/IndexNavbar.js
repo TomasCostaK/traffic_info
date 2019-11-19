4246071f5de4eb32352ad81d45029189fdccd516
Tom1k <tomascosta@ua.pt>
2019-11-19 11:22:20 +0000

    Changed basic map interface

--- traffic-jammer/Mapper/src/components/Navbars/IndexNavbar.js
+++ traffic-jammer/Mapper/src/components/Navbars/IndexNavbar.js
@@ -69,9 +69,9 @@ function IndexNavbar() {
             data-placement="bottom"
             href="/index"
             target="_blank"
-            title="Coded by Creative Tim"
+            title=""
           >
-            Paper Kit React
+            Traffic Jammer
           </NavbarBrand>
           <button
             aria-expanded={navbarCollapse}
@@ -91,67 +91,26 @@ function IndexNavbar() {
           isOpen={navbarCollapse}
         >
           <Nav navbar>
+            
             <NavItem>
               <NavLink
                 data-placement="bottom"
-                href="https://twitter.com/CreativeTim?ref=creativetim"
+                href="https://gitlab.com/myiesgroup/iesproject_trafficjammer"
                 target="_blank"
-                title="Follow us on Twitter"
+                title="Follow on GitLab"
               >
-                <i className="fa fa-twitter" />
-                <p className="d-lg-none">Twitter</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.facebook.com/CreativeTim?ref=creativetim"
-                target="_blank"
-                title="Like us on Facebook"
-              >
-                <i className="fa fa-facebook-square" />
-                <p className="d-lg-none">Facebook</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
-                target="_blank"
-                title="Follow us on Instagram"
-              >
-                <i className="fa fa-instagram" />
-                <p className="d-lg-none">Instagram</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.github.com/CreativeTimOfficial/paper-kit-react?ref=creativetim"
-                target="_blank"
-                title="Star on GitHub"
-              >
-                <i className="fa fa-github" />
-                <p className="d-lg-none">GitHub</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                href="https://demos.creative-tim.com/paper-kit-react/#/documentation?ref=pkr-index-navbar"
-                target="_blank"
-              >
-                <i className="nc-icon nc-book-bookmark" /> Documentation
+                <i className="fa fa-gitlab" />
+                <p className="d-lg-none">GitLab</p>
               </NavLink>
             </NavItem>
             <NavItem>
               <Button
                 className="btn-round"
-                color="danger"
-                href="#pablo"
+                color="primary"
+                href="/admin"
                 target="_blank"
-                disabled
               >
-                Upgrade to Pro
+                Admin Dashboard
               </Button>
             </NavItem>
           </Nav>
