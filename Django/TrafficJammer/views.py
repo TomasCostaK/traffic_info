52f1e7dbcca940c65950040464f49b6dd1d73dd7
Miguel Mota <miguel.mota@ua.pt>
2019-12-10 23:04:52 +0000

    Fixed bug that was created while manually testing with django admin feature. Created some integrity errors that made other code thrown errors where it should

--- Django/TrafficJammer/views.py
+++ Django/TrafficJammer/views.py
@@ -241,6 +241,6 @@ def roadblock(request):
         else:
             return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     except Section.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return HttpResponse("Section doesn't exist",status=status.HTTP_404_NOT_FOUND)
     except Blocked.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return HttpResponse("Road isn't blocked",status=status.HTTP_404_NOT_FOUND)
--- GenerateRoadblock.py
+++ GenerateRoadblock.py
@@ -2,20 +2,42 @@ import requests
 import json
 import random
 import time
-while True:
-    req=json.loads(requests.get('http://127.0.0.1:8000/info_street').content)
-    data={d['id']:True if d['transit_type']=='Blocked' else False for d in req }
-    time.sleep(5)
-    # Generating positive values to go negative and negative to go positive
-    data_positive=[d for d in data if data[d]==True]
-    data_negative=[d for d in data if data[d]==False]
+#while True:
+req=json.loads(requests.get('http://127.0.0.1:8000/info_street').content)
+data={d['id']:True if d['transit_type']=='Blocked' else False for d in req }
+time.sleep(5)
+print(data)
+# Generating positive values to go negative and negative to go positive
+data_positive=[d for d in data if data[d]==True] # Blocked
+data_negative=[d for d in data if data[d]==False] # Not Blocked
+random_data_positive=[]
+random_data_negative=[]
+if len(data_positive)!=0:
+    for i in range(min(5,len(data_positive))):
+        choice=random.choice(data_positive)
+        if choice not in random_data_positive:
+            random_data_positive.append(choice)
 
-    if len(data_positive)!=0:
-        random_data_positive=[random.choice(data_positive) for i in range(min(5,len(data_positive)))]
-    if len(data_negative)!=0:
-        random_data_negative=[random.choice(data_negative) for i in range(min(2,len(data_negative)))]
+if len(data_negative)!=0:
+    for i in range(min(2,len(data_negative))):
+        choice=random.choice(data_negative)
+        if choice not in random_data_negative:
+            random_data_negative.append(choice)
 
-    json_update_true=[json.dumps({"info":"roadblock_down","id":d}) for d in random_data_positive]
-    json_update_false=[json.dumps({"info":"roadblock_up","id":d}) for d in random_data_negative]
-    [print(i) for i in json_update_true]
-    [print(f) for f in json_update_false]
+json_update_true=[{"info":"roadblock_down","id":d} for d in random_data_positive] # Blocked
+json_update_false=[{"info":"roadblock_up","id":d} for d in random_data_negative] # Not blocked
+#[print(i) for i in json_update_true]
+#[print(f) for f in json_update_false]
+
+for i in json_update_true:
+    print(i)
+    #r=requests.delete("http://192.168.160.237:8000/roadblock/",data=json.dumps(i),headers={"Content-Type":"text/plain"})
+    r=requests.delete("http://127.0.0.1:8000/roadblock/",data=json.dumps(i),headers={"Content-Type":"text/plain"})
+
+    print(r)
+    print(r.text)
+for f in json_update_false:
+    print(f)
+    r=requests.put("http://127.0.0.1:8000/roadblock/",data=json.dumps(f),headers= {"Content-Type":"text/plain"})
+    print(r)
+    print(r.text)
